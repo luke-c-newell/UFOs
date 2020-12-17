430b91aea6e54e14d5eeae33c48df3aada521ba5
Luke Newell <lukenewell@lukes-mbp.home>
2020-12-17 12:14:05 -0600

    Updated app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,22 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+// Build a function to populate a table with data
+function buildTable(data) {
+    // Clear out any existing data
+    tbody.html("");
+
+    // Loop through each object and append a row and cells for each value
+    data.forEach((dataRow) => {
+        // Append a row to the table
+        let row = tbody.append("tr");
+        
+        // Loop through each field and add each value as a table cell
+        Object.values(dataRow).forEach((val) => {
+          let cell = row.append("td");
+          cell.text(val);
+          }
+        );
+      });
+    } 
